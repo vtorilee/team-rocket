d55eb43a95b4e2c4a215ab8be5bc4837f021fa98
vtorilee <victorialee_2003@hotmail.com>
2026-04-24 19:23:32 -0700

    minor vis fixes + feedback comments

--- types.js
+++ types.js
@@ -4,27 +4,24 @@ const nextBtn = document.getElementById("typeNextBtn");
 
 let selectedType = null;
 
-
-
 typeButtons.forEach(button => {
   button.addEventListener("click", () => {
     selectedType = button.innerText;
-
     
     resultBox.innerHTML = `
       <h2>You chose ${selectedType} type!</h2>
       
     `;
 
-   
     localStorage.setItem("selectedType", selectedType);
 
-    
     nextBtn.style.display = "inline-block";
   });
 });
 function sendHeight() {
-  const height = document.body.scrollHeight;
+  const chart = document.querySelector(".sunburst-vis");
+  const height = chart.scrollHeight;
+  window.parent.postMessage({ type: "setHeight", height }, "*");
 
   window.parent.postMessage({
     type: "setHeight",
